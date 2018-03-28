2b662fcac7f7b810b8b7a4cd7a7acdfed4d77e03
yaminmhd <mdyamin.drzee@gmail.com>
2018-03-28 11:36:24 +0800

    Added lodash debounce to delay input search by 300ms and remove unnecessary

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React, { Component } from "react";
 import "./App.css";
+import _ from "lodash";
 import SearchBar from "./components/SearchBar/SearchBar";
+import ErrorPage from "./components/ErrorPage/ErrorPage";
 import ProfileSection from "./components/ProfileSection/ProfileSection";
 import RepoSection from "./components/RepoSection/RepoSection";
 import Footer from "./components/Footer/Footer";
+
+const id = "0411e12b010f75a6c751";
+const secret = "8e1123577ee45b8e1f24df68d24f71f286f12224";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       profile: [],
-      repos: [],
-      repos_count: 5,
-      repos_sort: "created: asc",
-      id: "0411e12b010f75a6c751",
-      secret: "8e1123577ee45b8e1f24df68d24f71f286f12224"
+      repos: []
     };
   }
 
@@ -25,39 +26,46 @@ class App extends Component {
         repos: []
       });
     } else {
-      const profileResponse = await fetch(
-        `https://api.github.com/users/${term}?client_id=${
-          this.state.id
-        }&client_secret=${this.state.secret}`
-      );
+      try {
+        const profileResponse = await fetch(
+          `https://api.github.com/users/${term}?client_id=${id}&client_secret=${secret}`
+        );
 
-      const repoResponse = await fetch(
-        `https://api.github.com/users/${term}/repos?per_page=${
-          this.state.repos_count
-        }&sort=${this.state.repos_sort}&client_id=${
-          this.state.id
-        }&client_secret=${this.state.secret}`
-      );
+        const repoResponse = await fetch(
+          `https://api.github.com/users/${term}/repos?per_page=5
+          }&sort=created: asc
+          }&client_id=${id}&client_secret=${secret}`
+        );
 
-      const profile = await profileResponse.json();
-      const repos = await repoResponse.json();
-      this.setState({
-        profile,
-        repos
-      });
+        const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
+        this.setState({
+          profile,
+          repos
+        });
+      } catch (error) {
+        console.log(error);
+      }
     }
   }
 
   render() {
+    const search = _.debounce(term => {
+      this.inputSearch(term);
+    }, 300);
     const { profile, repos } = this.state;
     const inputNull =
       (profile.length === 0 || profile.message === "Not Found") &&
       (repos.length === 0 || repos.message === "Not Found");
     return (
       <div className="container">
-        <SearchBar onSearchTermChange={term => this.inputSearch(term)} />
-        {!inputNull && <ProfileSection profile={this.state.profile} />}
-        {!inputNull && <RepoSection repos={this.state.repos}/>}
+        <SearchBar onSearchTermChange={search} />
+        {!inputNull ? (
+          <ProfileSection profile={this.state.profile} />
+        ) : (
+          <ErrorPage />
+        )}
+        {!inputNull && <RepoSection repos={this.state.repos} />}
         <Footer />
       </div>
     );
