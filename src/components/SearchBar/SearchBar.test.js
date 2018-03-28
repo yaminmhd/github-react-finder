0e34e87e40d4c5d9dbb30e11b77b1a2b8c921be7
yaminmhd <mdyamin.drzee@gmail.com>
2018-03-28 22:48:59 +0800

    Added test for SearchBar component

--- src/components/SearchBar/SearchBar.test.js
+++ src/components/SearchBar/SearchBar.test.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import {shallow} from 'enzyme';
+import SearchBar from './SearchBar';
 
-describe('test', ()=>{
-  it('should behave...', () => {
+describe('SearchBar', ()=>{
+  it('should render SearchBar correctly', () => {
+    const wrapper = shallow(<SearchBar />);
+    expect(wrapper.find('h1')).toHaveLength(1);
+    expect(wrapper.find('p')).toHaveLength(1);
+    expect(wrapper.find('input')).toHaveLength(1);
+  });
 
+  it('should trigger the handleInputChange function on change', () => {
+    const mockHandler = jest.fn();
+    const wrapper = shallow(<SearchBar onSearchTermChange={mockHandler}/>);
+    wrapper.find('input').simulate('change', {target:{value:'a'}});
+    expect(wrapper.state().searchTerm).toEqual('a');
   });
 })
