b3de680f357d8761ed88bf1d05aa5e886d84a62e
yaminmhd <mdyamin.drzee@gmail.com>
2018-04-01 15:43:21 +0800

    Added test for Navbar component

--- src/components/NavBar/NavBar.test.js
+++ src/components/NavBar/NavBar.test.js
@@ -4,5 +4,13 @@ import NavBar from './NavBar.js';
 
 describe('NavBar', ()=>{
   it('should render NavBar correctly', () => {
+    const wrapper = shallow(<NavBar/>);
+    expect(wrapper.find('Navbar')).toHaveLength(1);
+  });
+
+  it('should toggle NavBar correctly', () => {
+    const wrapper = shallow(<NavBar/>);
+    wrapper.find('NavbarToggler').simulate('click');
+    expect(wrapper.state().isOpen).toEqual(true);
   });
 })
